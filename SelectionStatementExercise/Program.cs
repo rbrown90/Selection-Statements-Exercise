23fcc1436e3f342c0606931b8898b1f083b09ef6
Rakin Brown <rakinbrown38@gmail.com>
2023-04-11 16:57:06 -0500

    else - if statement update

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the guessing game!");
+            Console.WriteLine("Would you like to take a guess?");
+
+            var userResponse = Console.ReadLine();
+
+            Console.WriteLine("Let's get started!");
+            Console.WriteLine("Take your guess!");
+
+            var random = new Random();
+            var favNumber = random.Next(1,10);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber > userInput)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (favNumber < userInput)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
         }
     }
 }
